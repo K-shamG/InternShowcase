8deb38913ac30eb6f965402c9bb071a56118379e
keugnu <smfbrooks@gmail.com>
2017-07-21 21:13:45 -0500

    added routes

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app_route('/')
 def homepage():
     return render_template('index.html')
+
+@app_route('/india.html')
+def india():
+    return render_templates('india.html')
+
+@app_route('/about.html')
+def about():
+    return render_template('about.html')
+
 
 if name == '__main__':
     app.run()
